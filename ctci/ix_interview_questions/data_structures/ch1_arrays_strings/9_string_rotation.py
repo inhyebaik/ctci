da0f8d7585e8873ab7c2118fce28e00f8018d934
Inhye <inb125@mail.harvard.edu>
2018-01-11 14:16:14 -0800

    more string rotation

--- ctci/ix_interview_questions/data_structures/ch1_arrays_strings/9_string_rotation.py
+++ ctci/ix_interview_questions/data_structures/ch1_arrays_strings/9_string_rotation.py
@@ -1,17 +1,20 @@
-
-
+# O(N)
+# Space: O(N^2) 
 def string_rotation(s1, s2):
     for i in range(1, len(s1)):
-        substring1 = s1[:i]
+        substring1 = s1[:i] # string slicing is O(N^2)
         substring2 = s1[i:]
         if substring2 + substring1 == s2:
             return True
     return False
 
+# O(N)
 def is_substring(string, substring):
     return substring in string
 
+# O(N)
+# Space: O(N)
 def string_rotation2(s1, s2):
     if len(s1) == len(s2):
-        return is_substring(s1+s1, s2)
+        return is_substring(s1+s1, s2) # string concatentation is O(N)
     return False
