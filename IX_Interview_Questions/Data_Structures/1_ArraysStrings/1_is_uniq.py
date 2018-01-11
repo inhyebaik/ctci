bdddbef996a8812ba872107dddad374997ad9bfc
Inhye <inb125@mail.harvard.edu>
2018-01-10 23:42:21 -0800

    re do arrays/strings

---
+++ IX_Interview_Questions/Data_Structures/1_ArraysStrings/1_is_uniq.py
@@ -0,0 +1,21 @@
+# O(N)
+def is_uniq(string):
+    # Assuming ASCII characters
+    if len(string) > 128:
+        return False
+
+    char_set = [False for _ in range(128)]
+
+    for char in string:
+
+        # get ascii number of char
+        val = ord(char)
+        
+        if char_set[val]:
+            return False
+        else:
+            char_set[val] = True
+    
+    return True
+
+
