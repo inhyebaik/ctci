dc0ba3100d259a0b625828f2801524a51e2734a5
Inhye <inb125@mail.harvard.edu>
2018-01-13 15:06:02 -0800

    clear bit functinos

--- ctci/ix_interview_questions/data_structures/ch5_bit_manipulation/bit_operations.py
+++ ctci/ix_interview_questions/data_structures/ch5_bit_manipulation/bit_operations.py
@@ -2,4 +2,23 @@ def get_bit(num, i):
     """ Checks if num has an i-bit. """
     return ((num & (1 << i)) != 0)
 
-def 
+def set_bit(num, i):
+    """ Sets i-bit in num to 1. """
+    return (num | (1 << i))
+
+def clear_bit(num, i):
+    """ Clears i-bit in num.  """    
+    mask = ~(1 << i)
+    return num & mask
+
+def clear_bits_MSB_to_i(num, i):
+    """ Clears bits from most significant to i, inclusive. """
+    mask = (1 << i) - 1
+    return num & mask
+
+def clear_bits_0_to_i(num, i):
+    mask = -1 << (i + 1)
+    return num and mask
+
+def update_bit(num, i):
+    
