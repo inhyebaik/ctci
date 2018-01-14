a26f5b3a0d372c24d547637310650c495dfbf552
Inhye <inb125@mail.harvard.edu>
2018-01-13 17:42:03 -0800

    start on insert

--- ctci/ix_interview_questions/data_structures/ch5_bit_manipulation/bit_operations.py
+++ ctci/ix_interview_questions/data_structures/ch5_bit_manipulation/bit_operations.py
@@ -1,35 +1,27 @@
 def get_bit(num, i):
-    """ Checks if num has an i-bit. """
-    return ((num & (1 << i)) != 0)
+    """ Checks if ith bit is set (is 1) """
+    mask = 1 << i
+    return (num & mask) != 0
 
 def set_bit(num, i):
-    """ Sets i-bit in num to 1. """
-    return (num | (1 << i))
+    """ Sets ith bit to 1. """
+    mask = 1 << i
+    return (num | mask)
 
 def clear_bit(num, i):
-    """ Clears i-bit in num.  """    
+    """ Clears ith bit to 0. """
     mask = ~(1 << i)
     return num & mask
 
 def clear_bits_MSB_to_i(num, i):
-    """ Clears bits from most significant to i, inclusive. """
-    mask = (1 << i) - 1
+    """ Clears most significant bit to ith bit, inclusive. """
+    # make a sequence of 0s followed by i 1s
+    # clearing MSB is 
+    mask = (1 << i)-1
     return num & mask
 
-def clear_bits_0_to_i(num, i):
+def clear_bits_i_to_0(num, i):
+    """ Clears ith to 0th bits, inclusive. """
+    # make a sequence of 1s followed by i 0s 
     mask = -1 << (i + 1)
-    return num and mask
-
-def update_bit(num, i, value):
-    """ Set ith bit to value. """
-    if value == 0:
-        clear_bit(num, i)
-    else:
-        set_bit(num, i)
-
-def update_bit2(num, i, value):
-    """ Set ith bit to value. """
-    # clear ith bit with mask
-    mask = ~(1 << i)
-    # set ith bit with OR
-    return (num & mask) | (value << i)
+    return num & mask
