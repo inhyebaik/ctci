89efb5ba52309e43fbd520cf46e76cd3ee32f5f9
Inhye <inb125@mail.harvard.edu>
2018-01-14 11:49:05 -0800

    start 5.3

---
+++ ctci/ix_interview_questions/data_structures/ch5_bit_manipulation/3_flip.py
@@ -0,0 +1,9 @@
+# Flip Bit to Win: You have an integer and you can flip exactly one bit from a 13 
+# to a 1. Write code to find the length of the longest sequence of 1s you could 
+# create.
+
+# Ex.
+# Input: 1775 (or: 1110111101111) 
+# Output: 8
+
+def flip_bit_to_win(n):
